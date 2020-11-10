7b1b2c0cc742821668406512c632f6d71eb08cc2
BartDrown <bjakubowski.dev@gmail.com>
2020-11-10 23:40:00 +0100

    feat: animated navbar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('toggleClick', [     // trigger block
+      state('0', style({      // final CSS following animation
+        color: 'white',
+      })),
+      state('1', style({
+        color: '#F87356'
+      })),
+      state('2', style({
+        color: '#56F873'
+      })),
+      state('3', style({
+        color: '#7356F8'
+      })),
+      transition('0 => 1', animate(`2000ms linear`)),
+      transition('1 => 2', animate(`2000ms linear`)),
+      transition('2 => 3', animate(`2000ms linear`)),
+      transition('3 => 0', animate(`2000ms linear`)),
+    ])
+  ]
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit{
+  state: number = 0;
+
+  ngAfterViewInit(){
+    setInterval(()=>{
+      this.toggleIsCorrect();
+    },3000)
+
+  }
+
+  toggleIsCorrect() {
+    if(this.state < 3){
+    this.state ++;
+    return
+    }
+    this.state = 0;
+
+  }
 
 }
