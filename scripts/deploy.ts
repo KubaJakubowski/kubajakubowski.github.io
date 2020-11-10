d90c20c5945f82bd812fecc8e561384fcd6eb996
BartDrown <bjakubowski.dev@gmail.com>
2020-11-10 19:05:43 +0100

    fix: await exec

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,25 @@
-const { exec } = require('child_process');
+const exec = require('await-exec')
 
 export{}
 
 console.log('Depolying: ', `deploy-${Date.now()}`);
 
-exec('git checkout deploy');
+await exec('git checkout deploy');
 console.log(`Git: `, `checked out to deploy branch`);
 
-exec(`git pull`)
+await exec(`git pull`)
 console.log(`Git: `, `pulling changes`)
 
 console.log(`Angular: `, `starting building docs`);
 await exec(`npm run deploy-build`)
 console.log(`Angular: `, `finished building docs`);
 
-exec('git add .');
-exec(`git commit -m "chore: deploy-${Date.now()}"`);
-exec('git push');
+await exec('git add .');
+await exec(`git commit -m "chore: deploy-${Date.now()}"`);
+await exec('git push');
 console.log('Git: ', 'pushing build');
 
-exec('git checkout main');
+await exec('git checkout main');
 console.log('Git: ', 'checked out to main branch');
 
 
