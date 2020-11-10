b75a97cba1bafe83763938120abd3dd3de9b7370
BartDrown <bjakubowski.dev@gmail.com>
2020-11-10 19:46:56 +0100

    fix: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,28 +3,32 @@ const exec = require('await-exec')
 
 async function deploy(){
 
-console.log('Depolying: ', `deploy-${Date.now()}`);
+  console.log('Depolying: ', `deploy-${Date.now()}`);
 
-await exec('git checkout deploy');
-console.log(`Git: `, `checked out to deploy branch`);
+  await exec('git checkout deploy');
+  console.log(`Git: `, `checked out to deploy branch`);
 
-await exec(`git pull`)
-console.log(`Git: `, `pulling changes`)
+  await exec(`git pull`)
+  console.log(`Git: `, `pulling changes`)
 
-console.log(`Angular: `, `starting building docs`);
-await exec(`npm run deploy-build`)
-console.log(`Angular: `, `finished building docs`);
+  console.log(`Angular: `, `starting building docs`);
+  await exec(`npm run deploy-build`)
+  console.log(`Angular: `, `finished building docs`);
 
-await exec('git add .');
-exec(`git commit -m "chore: deploy-${Date.now()}"`);
-exec('git push');
-console.log('Git: ', 'pushing build');
+  setTimeout(()=> {
+    exec('git add .');
+    exec(`git commit -m "chore: deploy-${Date.now()}"`);
+  }, 1000)
 
-setTimeout(()=> {
-  exec('git checkout main');
-  console.log('Git: ', 'checked out to main branch');
-}, 1000)
+  setTimeout(()=> {
+    exec('git push');
+    console.log('Git: ', 'pushing build');
+  })
+
+  setTimeout(()=> {
+    exec('git checkout main');
+    console.log('Git: ', 'checked out to main branch');
+  })
 
 }
-
 deploy()
